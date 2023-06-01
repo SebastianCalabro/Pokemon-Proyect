f25112bb84e4b2c140dfc7987692440c14d60306
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-06-01 13:15:31 -0300

    test render deploy

--- api/src/db.js
+++ api/src/db.js
@@ -2,18 +2,26 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD } = process.env;
+const { DB_NAME, DB_HOST, DB_USER, DB_DEPLOY, DB_URL } = process.env;
 
-
+// BASE DE DATOS LOCAL
 /* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/pokemon`, {
   logging: false, 
   native: false, 
 }); */
 
+// LO COMENTADO ES COMO ANDABA ANTES
 const sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
+  /* process.env.NODE_ENV === "production"
+    ?  */new Sequelize(DB_DEPLOY,{
+        logging:false,
+        native:false,
+        dialectOptions: {
+          ssl: {
+            require:true,
+          }
+        }
+        /* database: DB_NAME,
         dialect: "postgres",
         host: DB_HOST,
         port: 5432,
@@ -24,12 +32,12 @@ const sequelize =
           ssl: { require: true, rejectUnauthorized: false },
           keepAlive: true,
         },
-        ssl: true,
+        ssl: true, */
       })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/pokemon`, {
+    /* : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/pokemon`, {
       logging: false, 
       native: false, 
-    });
+    }); */
 
 
 
